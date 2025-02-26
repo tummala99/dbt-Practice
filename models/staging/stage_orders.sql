09f19da61a070d649695931edebfb814aaf69d96
Srinivasa.tummala@xyenta.com <Srinivasa.tummala@xyenta.com>
2025-02-26 15:50:54 +0000

    Added macros

--- models/staging/stage_orders.sql
+++ models/staging/stage_orders.sql
@@ -17,9 +17,11 @@ select
       , p.category
       , p.productname
       , p.subcategory
+      , {{ markup('ordersellingprice','ordercostprice')}} as markup
    from {{ ref('raw_orders') }} as o 
    left join {{ ref('raw_customer') }} as c 
      on o.customerid = c.customerid
    left join {{ ref('raw_product') }} as p 
      on o.productid = p.productid
-
+     
+    --{{ limit_data_in_dev('shipdate')}}
